9497d5c0310b7d8576ff246668364f6df84445f9
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-02-04 12:55:54 +0530

    initial-commit

---
+++ models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const passportLocalMongoose = require('passport-local-mongoose')
+
+const UserSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true
+	},
+	mobile: {
+		type: Number,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
+})
+
+UserSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model('User', UserSchema)
